1607eb333fa1ae7274d228782ff72afd470e2c69
riteshNikam <riteshnikam2804@gmail.com>
2024-12-27 19:39:32 +0530

    added light version for the Premium.jsx

--- src/components/Premium.jsx
+++ src/components/Premium.jsx
@@ -1,59 +1,122 @@
 import { GiCheckMark } from "react-icons/gi";
+import { useSelector } from "react-redux";
 
 const Premium = () => {
+  const darkmodeFlag = useSelector(
+    (state) => state.darkmodeReducer.darkmodeFlag
+  );
+
   return (
     <>
-      <div className="dark:bg-black dark:text-white flex flex-row justify-center items-center py-10">
-        <h1 className="self-center text-2xl font-bold mx-5">
-          All Premium plans include
-        </h1>
-        <div className="mx-5 self-center">
-          <ul>
-            <li className="my-1 flex items-center">
-              <span className="mr-2">
-                <GiCheckMark />
-              </span>
-              <p>Add free music</p>
-            </li>
-            <li className="my-1 flex items-center">
-              <span className="mr-2">
-                <GiCheckMark />
-              </span>
-              <p>Download and listen online</p>
-            </li>
-            <li className="my-1 flex items-center">
-              <span className="mr-2">
-                <GiCheckMark />
-              </span>
-              <p>Play songs in any order</p>
-            </li>
-            <li className="my-1 flex items-center">
-              <span className="mr-2">
-                <GiCheckMark />
-              </span>
-              <p>High quality audio</p>
-            </li>
-            <li className="my-1 flex items-center">
-              <span className="mr-2">
-                <GiCheckMark />
-              </span>
-              <p>Listen with friends in real time</p>
-            </li>
-            <li className="my-1 flex items-center">
-              <span className="mr-2">
-                <GiCheckMark />
-              </span>
-              <p>Organise listening queue</p>
-            </li>
-            <li className="my-1 flex items-center">
-              <span className="mr-2">
-                <GiCheckMark />
-              </span>
-              <p>Listening insights (not in Mini)</p>
-            </li>
-          </ul>
+      {darkmodeFlag ? (
+        <div
+          className={`dark:bg-black dark:text-white flex flex-row justify-center items-center py-10`}
+        >
+          <h1 className="self-center text-2xl font-bold mx-5">
+            All Premium plans include
+          </h1>
+          <div className="mx-5 self-center">
+            <ul>
+              <li className="my-1 flex items-center">
+                <span className="mr-2">
+                  <GiCheckMark />
+                </span>
+                <p>Add free music</p>
+              </li>
+              <li className="my-1 flex items-center">
+                <span className="mr-2">
+                  <GiCheckMark />
+                </span>
+                <p>Download and listen online</p>
+              </li>
+              <li className="my-1 flex items-center">
+                <span className="mr-2">
+                  <GiCheckMark />
+                </span>
+                <p>Play songs in any order</p>
+              </li>
+              <li className="my-1 flex items-center">
+                <span className="mr-2">
+                  <GiCheckMark />
+                </span>
+                <p>High quality audio</p>
+              </li>
+              <li className="my-1 flex items-center">
+                <span className="mr-2">
+                  <GiCheckMark />
+                </span>
+                <p>Listen with friends in real time</p>
+              </li>
+              <li className="my-1 flex items-center">
+                <span className="mr-2">
+                  <GiCheckMark />
+                </span>
+                <p>Organise listening queue</p>
+              </li>
+              <li className="my-1 flex items-center">
+                <span className="mr-2">
+                  <GiCheckMark />
+                </span>
+                <p>Listening insights (not in Mini)</p>
+              </li>
+            </ul>
+          </div>
         </div>
-      </div>
+      ) : (
+        <div
+          className={`bg-white text-black flex flex-row justify-center items-center py-10`}
+        >
+          <h1 className="self-center text-2xl font-bold mx-5">
+            All Premium plans include
+          </h1>
+          <div className="mx-5 self-center">
+            <ul>
+              <li className="my-1 flex items-center">
+                <span className="mr-2">
+                  <GiCheckMark />
+                </span>
+                <p>Add free music</p>
+              </li>
+              <li className="my-1 flex items-center">
+                <span className="mr-2">
+                  <GiCheckMark />
+                </span>
+                <p>Download and listen online</p>
+              </li>
+              <li className="my-1 flex items-center">
+                <span className="mr-2">
+                  <GiCheckMark />
+                </span>
+                <p>Play songs in any order</p>
+              </li>
+              <li className="my-1 flex items-center">
+                <span className="mr-2">
+                  <GiCheckMark />
+                </span>
+                <p>High quality audio</p>
+              </li>
+              <li className="my-1 flex items-center">
+                <span className="mr-2">
+                  <GiCheckMark />
+                </span>
+                <p>Listen with friends in real time</p>
+              </li>
+              <li className="my-1 flex items-center">
+                <span className="mr-2">
+                  <GiCheckMark />
+                </span>
+                <p>Organise listening queue</p>
+              </li>
+              <li className="my-1 flex items-center">
+                <span className="mr-2">
+                  <GiCheckMark />
+                </span>
+                <p>Listening insights (not in Mini)</p>
+              </li>
+            </ul>
+          </div>
+        </div>
+      )}
     </>
   );
 };
